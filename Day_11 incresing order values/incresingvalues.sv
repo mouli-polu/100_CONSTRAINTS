c54ebc9c873c08d58a1080c715ea7c94611abff1
chanra mouli polu <chandramoulipolu09@gmail.com>
2025-04-05 01:05:19 +0530

    Update incresingvalues.sv
    Write a constraint to generate values in increasing order
    https://edaplayground.com/x/MnxD

--- Day_11 incresing order values/incresingvalues.sv
+++ Day_11 incresing order values/incresingvalues.sv
@@ -1,1 +1,27 @@
+// Code your testbench here
+// or browse Examples
+class increment ;
+  rand bit[5:0] incr_var[];
+  
+  constraint c1 { 
+    incr_var.size ==20;
+    foreach(incr_var[i]) 
+      if(i>0)
+        incr_var [i]> incr_var[i-1];    
+  }
+endclass
+ 
+module  tb;
+  increment obj;
+  initial begin
+    obj =new();
+    obj.randomize();
+    foreach (obj.incr_var[i]) begin
+      $display (" incr_var [%0d] =%0d", i, obj.incr_var[i]);
+    end
+  end
+endmodule
+      
+  
+  
 
